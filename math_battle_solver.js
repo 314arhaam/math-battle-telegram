36eb2b0abb8dce5028349faca531270f3d4f3f6a
314arham <prmbas@gmail.com>
2019-08-03 14:27:31 +0430

    Update math_battle_solver.js

--- math_battle_solver.js
+++ math_battle_solver.js
@@ -1,19 +1,30 @@
-function math_battle_solver(maxScore){
-	// Start the game
+function solve() {
+	var x = document.getElementById("task_x").innerHTML;
+	var y = document.getElementById("task_y").innerHTML;
+	var operator = document.getElementById("task_op").innerHTML;
+	var result = document.getElementById("task_res").innerHTML;
+	if (operator == "×"){
+		operator = "*"
+	}
+	else if (operator == "–"){
+		operator = "-"
+	}
+	var expression = x + operator + y
+	return Number(result) == eval(expression)
+}
+
+function main(maxScore) {
 	document.getElementById("button_correct").click()
 	for (i = 0; i < maxScore; i++){
-		var x = document.getElementById("task_x").innerHTML;
-		var y = document.getElementById("task_y").innerHTML;
-		var operator = document.getElementById("task_op").innerHTML;
-		var result = document.getElementById("task_res").innerHTML;
-		if (operator == "×"){
-			operator = "*"
+		if (solve()){
+			document.getElementById("button_correct").click()
 		}
-		else if (operator == "–"){
-			operator = "-"
+		else{
+			document.getElementById("button_wrong").click()	
 		}
-		var expression = x + operator + y
-		if (Number(result) == eval(expression)){
+	}
+	for (i = 0; i < 3; i++){
+		if (!solve()){
 			document.getElementById("button_correct").click()
 		}
 		else{
